d2cdfc6836c3c82e499c6734ea37c1771cfaf557
mymindinsane <arturguletskiy21@gmail.com>
2024-03-01 21:01:03 +0300

    steal some code

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int result = number % 10 * 100 + number / 10 % 10 * 10 + number / 100 ;
+        System.out.println(result);
     }
 }
